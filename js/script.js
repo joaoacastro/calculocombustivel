2d6460738161c1599a4cf4d7f1cc764d13929efc
JoaoACastro <joaoaccastro@gmail.com>
2024-10-18 17:00:20 -0300

    updated all code, styles, logic and responsivity

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 var btn = document.querySelector("#refresh");
-    btn.addEventListener("click", () => {
-    location.reload();
+btn.addEventListener("click", () => {
+  location.reload();
 });
 
-function calcular(){
-    let etanol = document.getElementById("etanol").value.replace(",",".");
-    let gasolina = document.getElementById("gasolina").value.replace(",",".");
-    let resultadoTexto = document.getElementById("resultadoTexto");
-    let resultado = document.getElementById("resultado");
+function calcular() {
+  let etanol = document.getElementById("etanol").value.replace(",", ".");
+  let gasolina = document.getElementById("gasolina").value.replace(",", ".");
+  
+  let autonomia = document.getElementById("autonomia").value.replace(",", ".");
+  
+  let resultado = document.getElementById("resultado");
 
-    let resultadoTemp = etanol/gasolina
+  let resultadoTemp = etanol / gasolina;
 
-    if (etanol <= 0 || gasolina <= 0){
-        alert("Ops, parece que faltou algo. \n Por favor, preencha tanto o valor do Etanol quanto da Gasolina!")
-        location.reload();
-    }
+  if (etanol <= 0 || gasolina <= 0 || autonomia <= 0) {
+    alert(
+      "Ops, parece que faltou algo. Mas calma que vamos te ajudar. \n Por favor, verifique se foi preenchido com valor positivo os campos do Etanol, da Gasolina e a Autonomia do veículo e tente novamente."
+    );
+    location.reload();
+  }
 
-    if (resultadoTemp < 0.7) {
-        resultadoTexto.innerHTML = "Obrigado por usar nossa plataforma, baseado nos valores inseridos o melhor neste momento, é abastecer com:";
-        resultado.innerHTML = "ETANOL";
+  if (resultadoTemp < 0.7) {
+    resultado.innerHTML = "ETANOL";
+    resultado.classList.replace("resultado", "resultadoText");
 
-        resultadoTexto.style = "padding: 10px; border-radius: 10px";
-        resultado.style = "padding: 10px; border-radius: 10px";
-        document.getElementById("refresh").disabled = false;
-    } else {
-        resultadoTexto.innerHTML = "Obrigado por usar nossa plataforma, baseado nos valores inseridos o melhor neste momento, é abastecer com:";
-        resultado.innerHTML = "GASOLINA";
+    document.getElementById("refresh").disabled = false;
+  } else {
+    resultado.innerHTML = "GASOLINA";
+    resultado.classList.replace("resultado", "resultadoText");
 
-        resultadoTexto.style = "padding: 10px; border-radius: 10px";
-        resultado.style = "padding: 10px; border-radius: 10px";
-        document.getElementById("refresh").disabled = false;
-    }
+    document.getElementById("refresh").disabled = false;
+  }
 }
